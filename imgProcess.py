e53cf8637438454fe69b86c5637b1cede77f378b
hiba zalloum <hebahzalloum@gmail.com>
2020-11-14 19:09:19 +0200

    Create Grey Level Function

--- imgProcess.py
+++ imgProcess.py
@@ -1,20 +1,29 @@
+import PIL
 from PIL import Image
 import os
 import sys
 
 
-class GreyImage:
+def convertGrey(img):
+    size = img.size
+    result = Image.new('RGB', size)
+    for i in range(size[0]):
+        for j in range(size[1]):
+            inPixel = img.getpixel((i, j))
+            red = inPixel[0]
+            green = inPixel[1]
+            blue = inPixel[2]
+            grey = (red + green + blue) // 3
+            result.putpixel((i, j), (grey, grey, grey))
+
+    result.show()
+
+def thmubnails():
     pass
-
-
-def convertGrey():
-    pass
-
-
-def thumbonails():
-    pass
-
 
 def histogram():
     pass
 
+annaImage = r'C:\Users\User\PycharmProjects\home work 4\anna.jpg'
+im = Image.open(annaImage)
+convertGrey(im)
